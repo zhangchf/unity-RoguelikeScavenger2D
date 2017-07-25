da44c0df0263d2a172111e794d3a511ec03b381e
Chaofan Zhang <chaofan.zhang@ef.com>
2017-07-25 14:40:35 +0800

    Create Player, Enemy, Floor, Wall, Food, Exit tiles, and procedural creation of level playfield.

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour {
+
+	[System.Serializable]
+	public class Count {
+		public int minimum;
+		public int maximum;
+
+		public Count(int minimum, int maximum) {
+			this.minimum = minimum;
+			this.maximum = maximum;
+		}
+	}
+
+	public int columns = 8;
+	public int rows = 8;
+	public Count wallCount = new Count (5, 9);
+	public Count foodCount = new Count (1, 5);
+	public GameObject exit;
+	public GameObject[] floorTiles;
+	public GameObject[] wallTiles;
+	public GameObject[] foodTiles;
+	public GameObject[] enemyTiles;
+	public GameObject[] outerWallTiles;
+
+	private Transform boardHolder;
+	private List<Vector2> gridPositions = new List<Vector2> ();
+
+
+	void InstantiateList() {
+		gridPositions.Clear ();
+		for (int i = 1; i < columns - 1; i++) {
+			for (int j = 1; j < rows - 1; j++) {
+				gridPositions.Add (new Vector2(i, j));
+			}
+		}
+	}
+
+	void BoardSetup() {
+		boardHolder = new GameObject ("Board").transform;
+
+		for (int x = -1; x < columns + 1; x++) {
+			for (int y = -1; y < rows + 1; y++) {
+				GameObject toInstantiate = floorTiles [Random.Range (0, floorTiles.Length)];
+				if (x == -1 || x == columns || y == -1 || y == rows) {
+					toInstantiate = outerWallTiles [Random.Range (0, outerWallTiles.Length)];
+				}
+
+				GameObject instance = Instantiate (toInstantiate, new Vector2 (x, y), Quaternion.identity);
+				instance.transform.SetParent (boardHolder);
+			}
+		}
+	}
+
+	Vector2 RandomPosition() {
+		int randomIndex = Random.Range (0, gridPositions.Count);
+		Vector2 randomPosition = gridPositions [randomIndex];
+		gridPositions.RemoveAt (randomIndex);
+		return randomPosition;
+	}
+
+	void LayoutObjectAtRandom(GameObject[] tileArray, int minimum, int maximum) {
+		int objectCount = Random.Range (minimum, maximum + 1);
+		for (int i = 0; i < objectCount; i++) {
+			Vector2 randomPosition = RandomPosition ();
+			Debug.Log ("RandomPosition=" + randomPosition);
+			GameObject tileChoice = tileArray [Random.Range (0, tileArray.Length)];
+			Instantiate (tileChoice, randomPosition, Quaternion.identity);
+		}
+	}
+		
+	public void SetupScene(int level) {
+		BoardSetup ();
+		InstantiateList ();
+		LayoutObjectAtRandom (wallTiles, wallCount.minimum, wallCount.maximum);
+		LayoutObjectAtRandom (foodTiles, foodCount.minimum, foodCount.maximum);
+		int enemyCount = (int)Mathf.Log (level, 2f);
+		Debug.Log ("EnemyCount:" + enemyCount);
+		LayoutObjectAtRandom (enemyTiles, enemyCount, enemyCount);
+		Instantiate (exit, new Vector2 (columns - 1, rows - 1), Quaternion.identity);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
